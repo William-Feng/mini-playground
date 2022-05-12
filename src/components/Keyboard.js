134169fe6a1b9bf3326d128e35086dfbadff3b97
William Feng <williamfeng2001@gmail.com>
2022-05-12 20:45:38 +1000

    Added typing letters functionality

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
+import { AppContext } from "../App";
 import Key from "./Key";
 
 function Keyboard() {
+  const { onLetter, onEnter, onDelete } = useContext(AppContext);
+
   const row1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const row2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const row3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const handleKeyboard = useCallback((e) => {
+    if (e.key === "Enter") {
+      onEnter();
+    } else if (e.key === "Backspace") {
+      onDelete();
+    } else {
+      const letters = [...row1, ...row2, ...row3];
+      letters.forEach((key) => {
+        if (e.key.toUpperCase() === key) {
+          onLetter(key);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="row">
         {row1.map((key) => {
           return <Key val={key} />;
