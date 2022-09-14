4a29c37b52ba55f901965a3197944d0ca461850c
William Feng <williamfeng2001@gmail.com>
2022-09-14 14:12:11 +1000

    Solidify logic for initialising the board and generating numbers

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-const cells = document.querySelectorAll(".cell");
+const cells = [...document.querySelectorAll(".cell")];
 const scoreMessage = document.getElementById("score");
 const restart = document.getElementById("restart");
 
-const ROWS = 4;
-const COLUMNS = 4;
-let board;
+const SIZE = 4;
+let board = [];
+let score;
 
 newGame();
 restart.addEventListener("click", newGame);
 
 function newGame() {
+  score = 0;
+  tempBoard = [];
+
+  // Initialise all cells within the 2D board to be empty
   cells.forEach((cell) => {
-    cell.classList.value = "cell";
+    cell.className = "cell";
     cell.innerText = "";
+    tempBoard.push(cell.innerText);
   });
-  board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
+  while (tempBoard.length) {
+    board.push(tempBoard.splice(0, SIZE));
+  }
+
+  scoreMessage.innerText = "Score: " + score;
+
+  generateCell();
+  generateCell();
+}
+
+// Generate a new number within the board
+function generateCell() {
+  while (true) {
+    row = parseInt(Math.random() * SIZE);
+    column = parseInt(Math.random() * SIZE);
+
+    if (board[row][column] == "") {
+      cell = getCell(row, column);
+
+      // 10% chance of generating a 4, 90% chance of generating a 2
+      chance = parseInt(Math.random() * 10);
+      if (chance == 0) {
+        board[row][column] = 4;
+        cell.innerText = 4;
+        cell.classList.add("num4");
+      } else {
+        board[row][column] = 2;
+        cell.innerText = 2;
+        cell.classList.add("num2");
+      }
+      return;
+    }
+  }
+}
+
+// Return the cell with corresponding id based on the row and column provided
+function getCell(row, column) {
+  return document.getElementById(row.toString() + "-" + column.toString());
 }
