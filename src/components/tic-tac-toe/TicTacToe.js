6a99390b33fe80940c7d57bf9d8143d81d7e543e
William Feng <williamfeng2001@gmail.com>
2022-09-18 12:41:55 +1000

    Refactor board setup logic with React

--- src/components/tic-tac-toe/TicTacToe.js
+++ src/components/tic-tac-toe/TicTacToe.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./TicTacToe.css";
 
 function TicTacToe() {
-  // const boardCells = document.querySelectorAll("[board-cell]");
+  const [cells, setCells] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState("X");
   const [winner, setWinner] = useState(null);
 
@@ -17,87 +17,52 @@ function TicTacToe() {
     [2, 4, 6],
   ];
 
-  function newGame() {
+  const handleRestart = () => {
+    setCells(Array(9).fill(null));
     setTurn("X");
     setWinner(null);
-    /*
-    boardCells.forEach((cell) => {
-      console.log("HELLO");
-      cell.classList.remove("nought", "cross");
-      cell.innerText = "";
-      cell.addEventListener("click", handleClick, { once: true });
-    });
-    */
-  }
+  };
 
-  useEffect(() => {
-    newGame();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  /*
-  function handleClick(e) {
-    if (!gameOver) {
-      // Place a nought or cross on the cell, and include the class
-      const cell = e.target;
-      const currentClass = xTurn ? "cross" : "nought";
-      cell.innerText = xTurn ? "X" : "O";
-      cell.classList.add(currentClass);
-
-      // Check for win and draw states, otherwise switch turns
-      if (checkWin(currentClass)) {
-        setMessage(`Player ${xTurn ? "X" : "O"} wins!`);
-        setGameOver(true);
+  const handleClick = (i) => {
+    if (!winner && !cells[i]) {
+      cells[i] = turn === "X" ? "X" : "O";
+      setCells(cells);
+      setTurn(turn === "X" ? "O" : "X");
+      /*
+      if (checkWin()) {
+        setWinner(turn)
       } else if (checkDraw()) {
-        setMessage("Draw!");
+        setWinner("draw")
       } else {
-        setXTurn(!xTurn);
+        setTurn(turn === "X" ? "O" : "X");
       }
+      */
     }
-  }
-
-  // If all the indexes within any one of the provided states contain the same class
-  function checkWin(currentClass) {
-    return WIN_STATES.some((state) => {
-      return state.every((index) => {
-        return boardCells[index].classList.contains(currentClass);
-      });
-    });
-  }
-
-  // If every cell is filled with either a nought or cross
-  function checkDraw() {
-    return [...boardCells].every((cell) => {
-      return (
-        cell.classList.contains("cross") || cell.classList.contains("nought")
-      );
-    });
-  }
-*/
+  };
 
   return (
     <div className="container">
       <h1>Tic-Tac-Toe</h1>
       <div className="board">
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
-        <div className="cell board-cell"></div>
+        {cells.map((value, i) => (
+          <div
+            className={value ? "cell occupied" : "cell"}
+            key={i}
+            onClick={() => handleClick(i)}
+          >
+            {value}
+          </div>
+        ))}
       </div>
       <div>
         {!winner ? (
-          <h2>Player {turn}'s Turn!</h2>
+          <h2>Player {turn}'s Turn</h2>
         ) : winner === "draw" ? (
           <h2>Draw!</h2>
         ) : (
           <h2>Player {winner} wins!</h2>
         )}
-        <button id="restart" onClick={() => newGame()}>
+        <button className="restart" onClick={() => handleRestart()}>
           Restart
         </button>
       </div>
