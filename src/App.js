21d4f9fdb6f79b30bb1a6648a5a0d1eb4f8f271d
William Feng <williamfeng2001@gmail.com>
2022-09-26 11:36:00 +1000

    Include mobile responsiveness for all pages

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         setWindowWidth(window.innerWidth)
       );
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [[], windowWidth]);
 
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../App";
 import "./Footer.css";
 
-function Footer() {
+function Footer({ id }) {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div className="footer" id={theme}>
+    <div className={"footer" + (id ? " " + id : "")} id={theme}>
       Created by <span className="author">William Feng</span> 2022
     </div>
   );
