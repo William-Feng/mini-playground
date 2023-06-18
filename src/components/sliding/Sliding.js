a506d54910225e6266d81fa07e8169629b18984b
William Feng <williamfeng2001@gmail.com>
2023-06-18 14:38:41 +1000

    Enable arrow key compatibility for Sliding Puzzle

--- src/components/sliding/Sliding.js
+++ src/components/sliding/Sliding.js
@@ -10,10 +10,11 @@ function Sliding() {
   const [board, setBoard] = useState([]);
   const [moves, setMoves] = useState(0);
   const [solved, setSolved] = useState(false);
-  const size = difficulty === "easy" ? 3 : 4;
+
+  const size = useMemo(() => (difficulty === "easy" ? 3 : 4), [difficulty]);
 
   useEffect(() => {
-    setBoard(gameInitialisation());
+    gameInitialisation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [difficulty]);
 
@@ -41,7 +42,7 @@ function Sliding() {
   const gameInitialisation = () => {
     setMoves(0);
     setSolved(false);
-    return shuffleBoard();
+    setBoard(shuffleBoard());
   };
 
   // Randomise the order of the cells (Fisher-Yates shuffle algorithm)
@@ -104,6 +105,7 @@ function Sliding() {
     return inversions;
   };
 
+  // Find the empty cell in the board to determine whether a move is valid
   const findEmptyCell = (board) => {
     for (let i = 0; i < size; i++) {
       for (let j = 0; j < size; j++) {
@@ -115,37 +117,68 @@ function Sliding() {
     return null;
   };
 
+  // Swap the empty cell with the target cell
+  const swapCells = (row, col, x, y) => {
+    const newBoard = board.map((row) => [...row]);
+    newBoard[x][y] = board[row][col];
+    newBoard[row][col] = "";
+    setBoard(newBoard);
+    setMoves(moves + 1);
+  };
+
+  // Check if the clicked cell is cardinally adjacent to the empty cell, then swap cells
   const handleClick = (row, col) => {
     if (solved) return;
+    const empty_cell = findEmptyCell(board);
     const directions = [
       [1, 0],
       [-1, 0],
       [0, 1],
       [0, -1],
     ];
-    const empty_cell = findEmptyCell(board);
     for (const [i, j] of directions) {
-      const x = row + i;
-      const y = col + j;
+      const [x, y] = [row + i, col + j];
       if (x === empty_cell[0] && y === empty_cell[1]) {
-        const newBoard = [...board];
-        newBoard[x][y] = board[row][col];
-        newBoard[row][col] = "";
-        setBoard(newBoard);
-        setMoves(moves + 1);
+        swapCells(row, col, x, y);
         break;
       }
     }
   };
 
-  useEffect(() => {
-    const checkWon = () => {
-      if (JSON.stringify(board) === JSON.stringify(solution)) {
-        setSolved(true);
-      }
+  // Check if the pressed key is a valid arrow key, then swap cells
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleKeyPress = (e) => {
+    if (solved) return;
+    const empty_cell = findEmptyCell(board);
+
+    const directions = {
+      ArrowUp: [1, 0],
+      ArrowDown: [-1, 0],
+      ArrowLeft: [0, 1],
+      ArrowRight: [0, -1],
     };
-    checkWon();
-  }, [board, solution]);
+    const offset = directions[e.key];
+    if (!offset) return;
+
+    const [row, col] = [empty_cell[0] + offset[0], empty_cell[1] + offset[1]];
+    if (row < 0 || row >= size || col < 0 || col >= size) return;
+    swapCells(row, col, empty_cell[0], empty_cell[1]);
+  };
+
+  // Check if the board is solved whenever it's updated
+  useEffect(() => {
+    if (JSON.stringify(board) === JSON.stringify(solution)) {
+      setSolved(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [board]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   return (
     <div
@@ -175,10 +208,7 @@ function Sliding() {
       <div className="message">
         {solved && <h2>Well done!</h2>}
         <h3>Moves: {moves}</h3>
-        <button
-          className="restart"
-          onClick={() => setBoard(gameInitialisation)}
-        >
+        <button className="restart" onClick={gameInitialisation}>
           Restart
         </button>
       </div>
