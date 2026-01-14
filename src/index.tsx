cef0a49a943067357ac8d8dd873e6a1283533b58
William Feng <williamfeng2001@gmail.com>
2026-01-14 23:39:18 +1100

    Convert src/index.js to src/index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "Failed to find the root element. Make sure there is an element with id='root' in your HTML."
+  );
+}
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
